1ccaad337ecd29d455412cfcea00aca7dc22d90b
Dmitri Babaev <dmitri.babaev@gmail.com>
2022-07-28 18:47:08 +0300

    data url fix

--- experiments/scenario_bowl2019/bin/get-data.sh
+++ experiments/scenario_bowl2019/bin/get-data.sh
@@ -2,6 +2,6 @@
 
 mkdir data
 
-wget https://getfile.dokpub.com/yandex/get/https://yadi.sk/d/31erlljbW6Px9A -O data-science-bowl-2019.zip
+wget https://storage.yandexcloud.net/di-datasets/data-science-bowl-2019.zip -O data-science-bowl-2019.zip
 unzip -j data-science-bowl-2019.zip -d data
 mv data-science-bowl-2019.zip data/
