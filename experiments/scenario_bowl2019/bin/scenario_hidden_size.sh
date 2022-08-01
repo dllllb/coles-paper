3442810073ff50575d8fc0673f0326742da0652e
Ivan A Kireev <ivkireev@yandex.ru>
2022-08-01 10:42:18 +0300

    fix run path

--- experiments/scenario_bowl2019/bin/scenario_hidden_size.sh
+++ experiments/scenario_bowl2019/bin/scenario_hidden_size.sh
@@ -62,7 +62,7 @@ python ../../ml_inference.py \
 # Compare
 rm results/scenario_bowl2019__hidden_size.txt
 # rm -r conf/embeddings_validation.work/
-LUIGI_CONFIG_PATH=conf/luigi.cfg python -m embeddings_validation \
+PYTHONPATH=../.. LUIGI_CONFIG_PATH=conf/luigi.cfg python -m embeddings_validation \
     --conf conf/embeddings_validation_short.hocon --workers 10 --total_cpu_count 20 \
     --conf_extra \
       'report_file: "../results/scenario_bowl2019__hidden_size.txt",
