2538f0b2b00fb91dbfe363b2a18c3bb6d6f29bbf
Dmitri Babaev <dmitri.babaev@gmail.com>
2024-02-20 18:35:48 +0300

    huggingface datasets

--- experiments/scenario_bowl2019/bin/get-data.sh
+++ experiments/scenario_bowl2019/bin/get-data.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir data
+cd data
 
-wget https://storage.yandexcloud.net/di-datasets/data-science-bowl-2019.zip -O data-science-bowl-2019.zip
-unzip -j data-science-bowl-2019.zip -d data
-mv data-science-bowl-2019.zip data/
+curl -OL 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train_labels.csv.gz?download=true'
+curl -OL 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/train.csv.gz?download=true'
+curl -OL 'https://huggingface.co/datasets/dllllb/datascience-bowl2019/resolve/main/test.csv.gz?download=true'
+
+gunzip -f *.csv.gz
